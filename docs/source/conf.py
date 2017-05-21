3ea28dda39ec440d4b995246ac760f83b1ea1092
Python3pkg <raliclo@gmail.com>
2017-05-21 01:54:58 -0700

    Convert to Python3

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -40,8 +40,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'ColanderAlchemy'
-copyright = u'2012, Stefano Fontanelli'
+project = 'ColanderAlchemy'
+copyright = '2012, Stefano Fontanelli'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -183,8 +183,8 @@ latex_elements = {
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-  ('index', 'ColanderAlchemy.tex', u'ColanderAlchemy Documentation',
-   u'Stefano Fontanelli', 'manual'),
+  ('index', 'ColanderAlchemy.tex', 'ColanderAlchemy Documentation',
+   'Stefano Fontanelli', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -213,8 +213,8 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'colanderalchemy', u'ColanderAlchemy Documentation',
-     [u'Stefano Fontanelli'], 1)
+    ('index', 'colanderalchemy', 'ColanderAlchemy Documentation',
+     ['Stefano Fontanelli'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -227,8 +227,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'ColanderAlchemy', u'ColanderAlchemy Documentation',
-   u'Stefano Fontanelli', 'ColanderAlchemy', 'One line description of project.',
+  ('index', 'ColanderAlchemy', 'ColanderAlchemy Documentation',
+   'Stefano Fontanelli', 'ColanderAlchemy', 'One line description of project.',
    'Miscellaneous'),
 ]
 
